fd760cfffb19d46115557020487ef4ee34657ec3
mrrukkus <doooog1@rambler.ru>
2020-06-13 21:22:52 +0500

    module2-task1

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Main from "../components/main.jsx";
+import PropTypes from "prop-types";
+
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {title, genre, year} = props;
+  const {title, genre, year, filmsTitles} = props;
 
   return (
-    <Main title={title} genre={genre} year={year}/>
+    <Main title={title} genre={genre} year={year} filmsTitles={filmsTitles}/>
   );
+};
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  filmsTitles: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default App;
